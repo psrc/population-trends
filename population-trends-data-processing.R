d6cc42195aaecf4180a5b38e0a5ef998fbc7bc24
mwkgj <110430835+mwkgj@users.noreply.github.com>
2024-06-21 11:56:19 -0700

    updated code and new charts

--- population-trends-data-processing.R
+++ population-trends-data-processing.R
@@ -263,3 +263,20 @@ county_2024_pop_change_chart_bar <- create_bar_chart(df = ofm_pop |>
                                                     x = "geography", y = "share_region_change", fill = "metric", 
                                                     bar_column = "bar", dec = 1,
                                                     esttype = "percent", color = c("#91268F"), legend = FALSE, left_align='25%', bottom_padding=50)
+
+
+# share of the regional total by county since 2018
+county_pop_share_chart <- create_bar_chart(df = ofm_pop |> 
+                                                      filter(regional_geography == "County" & year >= base_year) |>
+                                                      filter(geography != "Region") |>
+                                             mutate(year = as.character(year), metric = "County Share of Regional Total Since 2018"),
+                                                    x = "year", y = "share_region_total", fill = "geography", dec = 1,
+                                                    esttype = "percent", color = c("#91268F", "#F05A28", "#8CC63E", "#00A7A0"), left_align='10%', bottom_padding=75)
+
+# share of the regional change by county since 2018
+county_pop_change_chart <- create_bar_chart(df = ofm_pop |> 
+                                                       filter(regional_geography == "County" & year >= base_year) |>
+                                                       filter(geography != "Region") |>
+                                                      mutate(year = as.character(year), metric = "County Share of Population Change since 2018"),
+                                                     x = "year", y = "share_region_change", fill = "geography", dec = 1,
+                                                     esttype = "percent",  color = c("#91268F", "#F05A28", "#8CC63E", "#00A7A0"), left_align='10%', bottom_padding=75)
